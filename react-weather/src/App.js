a18d55f68253dd9b444a1d134bfcb1d77d29cb08
wzht1991 <506486189@qq.com>
2019-04-03 17:51:38 +1000

    footer

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from'./weather/header';
+import Footer from './weather/footer'
 import './style/main.css';
 class App extends Component {
   render() {
     return (
       <div class="weather-channel__container">
       <Header/>
+      <Footer/>
       </div>
     );
   }
---
+++ react-weather/src/weather/footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function footer(props){
+    return(
+        <footer class="weather-channel__footer"><p>Powered by React</p></footer>
+    )
+}
