b93d3c0f8e7a55dfd976848d24f8f4f05fb33b9d
wzht1991 <506486189@qq.com>
2019-04-03 17:26:48 +1000

    header_no_images

---
+++ react-weather/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from'./weather/header'
+class App extends Component {
+  render() {
+    return (
+      <div class="weather-channel__container">
+      <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-weather/src/weather/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function header(props){
+    return(
+        <header>
+        <img className="header__logo" src="icon/logo.png" />
+        <h1 className="header__title">Weather Channel</h1>
+      </header>
+    )
+}
